944d52895b4ac7d99cad2e0b9adab023e036fbcd
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-04-27 15:09:11 +0800

    dashboard and logout feature

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,12 +3,19 @@ import { Link } from "react-router-dom"
 
 export default function Dashboard() {
   return (
-    <div className="container border-solid bg-green-500 flex justify-center flex-col items-center">
-      <h1>dashboard on the left section.</h1>
-      <h1>create lobby button on the bottom right</h1>
-    <div >
+    <div class="flex h-screen">
+      <div class="w-4/5 bg-blue-200">
+        <h1>dashboard on the left section.</h1>
+      </div>
+      <div class="w-1/5 bg-green-200">
+
+        <h1>create lobby button on the bottom right</h1>
+        <Link to={"/login"}>
+            <button className="rounded-md px-6 py-2 bg-blue-500 text-white ">Log Out</button>
+        </Link>
+
         <Link to={"/userprofile"}>
-          <button className="bg-blue-500 text-white px-6 py-2 rounded-md">Profile</button>
+              <button className="bg-blue-500 text-white px-6 py-2 rounded-md">Profile</button>
         </Link>
       </div>
     </div>
