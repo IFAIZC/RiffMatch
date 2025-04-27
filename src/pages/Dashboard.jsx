522cdf8f5c12f0b5a5c0ec46285d32700082d086
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-04-27 23:34:43 +0800

    working on dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,27 @@
 import UserProfile from "./UserProfile"
-import { Link } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 export default function Dashboard() {
   return (
     <div class="flex h-screen">
       <div class="w-4/5 bg-blue-200">
-        <h1>dashboard on the left section.</h1>
+        <h1 className="flex justify-center">RiffMatch</h1>
+        {/* will display listed lobbies */}
+        <Outlet/>
       </div>
-      <div class="w-1/5 bg-green-200">
 
-        <h1>create lobby button on the bottom right</h1>
+      <div class="w-1/5 bg-green-200 flex flex-col justify-around items-center">
+        <Link to={"/userprofile"}>
+              <button className="bg-blue-500 text-white px-6 py-2 rounded-md">Profile</button>
+        </Link>
+
         <Link to={"/login"}>
             <button className="rounded-md px-6 py-2 bg-blue-500 text-white ">Log Out</button>
         </Link>
 
-        <Link to={"/userprofile"}>
-              <button className="bg-blue-500 text-white px-6 py-2 rounded-md">Profile</button>
+
+        <Link to={"/createLobby"}>
+              <button className="bg-blue-500 text-white px-6 py-2 rounded-md">Create Lobby</button>
         </Link>
       </div>
     </div>
