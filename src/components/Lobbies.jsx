75ac7756f69da5080604ad7899032ece9bd48338
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-05-03 23:59:59 +0800

    dashboard ui fix

--- src/components/Lobbies.jsx
+++ src/components/Lobbies.jsx
@@ -28,20 +28,22 @@ export default function Lobbies() {
 
 
   return (
-    <div className="flex flex-col justify-items-start items-center h-full gap-2">
+    <div className="flex flex-col items-center justify-center h-screen">
     {/* rendering data from lobbies */}
 
     {lobbies.length === 0 ? (
-      <p>Fetching Lobbies...</p>
+      <div className="flex items-center justify-center w-full h-full">
+        <p>Fetching Lobbies...</p>
+      </div>
     ) : (
-      <ul>
+      <ul className="flex flex-row overflow-x-auto whitespace-nowrap w-full px-4">
       {lobbies.map((lobby) => (
-        <li key={lobby.id} className="border-1 m-5 p-10 flex flex-col rounded-2xl">
+        <li key={lobby.id} className="border-1 m-2 p-10 flex flex-col rounded-2xl max-w-90 min-w-90 max-h-150 min-h-150">
           <strong>{lobby.name}</strong> - {lobby.description}
           <button
               className="bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-600">
               Join Lobby
-            </button>
+          </button>
         </li>
       ))}
     </ul>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,36 +7,55 @@ export default function Dashboard({user}) {
   const {signOut} = useAuth()
   
   return (
-    <div className="flex h-screen">
-      <div className="w-4/5 bg-blue-200">
-        <h1 className="flex justify-items-start m-1 text-2xl">RiffMatch</h1>
-        {/* will display listed lobbies */}
-        <Lobbies/>
-      </div>
+    <div className="h-screen overflow-y-hidden">
+      <div className="flex flex-row justify-between m-2">
 
-      <div className="w-1/5 bg-green-200 flex flex-col justify-around items-center">
+        <div className="text-2xl">RIFFMATCH</div>
 
-      <div className="flex justify-center mb-6">
+        <div className="flex gap-2">
+          <Link to={"/lobbyCreation"}>
+           <button className="bg-blue-500 text-white hover:bg-blue-600 px-6 py-2 rounded-2xl">Create Lobby</button>
+          </Link>
           <img
             src={user?.user_metadata?.picture}
             alt=""
-            className="rounded-full w-32 h-32 object-cover border-4 border-blue-500"
-          />
+            className="rounded-full w-10 h-10 object-cover"/>
         </div>
-
-      <p className="text-xl font-semibold text-center">{user?.user_metadata?.name}</p>
-
-
-        <Link to={"/userprofile"}>
-              <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md">Profile</button>
-        </Link>
-
-        <button onClick={signOut} className="bg-red-500 text-white px-6 py-2 rounded-md shadow-md hover:bg-red-600">Log Out</button>
-
-        <Link to={"/lobbyCreation"}>
-              <button className="bg-blue-500 text-white hover:bg-blue-600 px-6 py-2 rounded-md">Create Lobby</button>
-        </Link>
       </div>
+
+        <Lobbies/>
     </div>
   )
 }
+
+// <div className="flex h-screen">
+//   <div className="w-4/5 bg-blue-200">
+//     <h1 className="flex justify-items-start m-1 text-2xl">RiffMatch</h1>
+//     {/* will display listed lobbies */}
+//     <Lobbies/>
+//   </div>
+
+//   <div className="w-1/5 bg-green-200 flex flex-col justify-around items-center">
+
+//   <div className="flex justify-center mb-6">
+//       <img
+//         src={user?.user_metadata?.picture}
+//         alt=""
+//         className="rounded-full w-32 h-32 object-cover border-4 border-blue-500"
+//       />
+//     </div>
+
+//   <p className="text-xl font-semibold text-center">{user?.user_metadata?.name}</p>
+
+
+//     <Link to={"/userprofile"}>
+//           <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md">Profile</button>
+//     </Link>
+
+//     <button onClick={signOut} className="bg-red-500 text-white px-6 py-2 rounded-md shadow-md hover:bg-red-600">Log Out</button>
+
+//     <Link to={"/lobbyCreation"}>
+//           <button className="bg-blue-500 text-white hover:bg-blue-600 px-6 py-2 rounded-md">Create Lobby</button>
+//     </Link>
+//   </div>
+// </div>
