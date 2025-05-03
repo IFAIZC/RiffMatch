738a992764018b06441f4b69193b9fae3b62d525
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-05-03 22:46:01 +0800

    planning to redesign dashboard UI

--- src/components/Lobbies.jsx
+++ src/components/Lobbies.jsx
@@ -36,7 +36,7 @@ export default function Lobbies() {
     ) : (
       <ul>
       {lobbies.map((lobby) => (
-        <li key={lobby.id} className="border-1 m-5 p-10 flex flex-col">
+        <li key={lobby.id} className="border-1 m-5 p-10 flex flex-col rounded-2xl">
           <strong>{lobby.name}</strong> - {lobby.description}
           <button
               className="bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-600">
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,14 +9,23 @@ export default function Dashboard({user}) {
   return (
     <div className="flex h-screen">
       <div className="w-4/5 bg-blue-200">
-        <h1 className="flex justify-center">RiffMatch</h1>
+        <h1 className="flex justify-items-start m-1 text-2xl">RiffMatch</h1>
         {/* will display listed lobbies */}
         <Lobbies/>
       </div>
 
       <div className="w-1/5 bg-green-200 flex flex-col justify-around items-center">
 
+      <div className="flex justify-center mb-6">
+          <img
+            src={user?.user_metadata?.picture}
+            alt=""
+            className="rounded-full w-32 h-32 object-cover border-4 border-blue-500"
+          />
+        </div>
+
       <p className="text-xl font-semibold text-center">{user?.user_metadata?.name}</p>
+
 
         <Link to={"/userprofile"}>
               <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md">Profile</button>
