8668adae71806c62b0363527f015befbf4e4db12
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-05-31 01:24:43 +0800

    fixed main menu dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,51 +2,56 @@ import UserProfile from "./UserProfile"
 import { Link, Outlet } from "react-router-dom"
 import useAuth from "../hooks/useAuth"
 import Lobbies from "../components/Lobbies"
-import { useState } from "react"
 
 export default function Dashboard({user}) {
   const {signOut} = useAuth()
-  const [dropDown,setDropDown] = useState(false)
-
-  // drop down menu on profile
-  function toggleMenu() {
-    setDropDown((prev) => !prev)
-  }
   
   return (
+    // might need to remove this, changing from veritcal scrolling to horizantal!!
     <div className="h-screen overflow-y-hidden ">
-      <div className="flex flex-row justify-between m-2">
 
-        <div className="text-2xl">RIFFMATCH</div>
+    <div className="navbar bg-base-100 shadow-sm">
 
-        <div className="flex gap-2">
-          <Link to={"/lobbyCreation"}>
-           <button className="bg-blue-500 text-white hover:bg-blue-600 px-6 py-2 rounded-2xl">Create Lobby</button>
-          </Link>
-
-          <div className="relative">
-            <button className="cursor-pointer" onClick={toggleMenu}>
-              <img
-              src={user?.user_metadata?.picture}
-              alt=""
-              className="rounded-full w-10 h-10 object-cover"
-              />
-            </button>
-
-            {dropDown && (
-              <ul className="absolute right-0  w-40 bg-gray-200 shadow-md rounded-lg py-2 z-50">
-              <Link to={"/userprofile"}>
-              <li className="px-4 py-2 cursor-pointer">View Profile</li>
-              </Link>
-              <li className="px-4 py-2 cursor-pointer" onClick={signOut}>Log Out</li>
-              </ul>
-            )}
-          </div>
-
-
-        </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">RIFFMATCH</a>
       </div>
 
+      <div className="flex-none">
+          <Link to={"/lobbyCreation"}>
+            <button className="btn btn-primary rounded-xl">Create Lobby</button>
+          </Link>
+          
+          <div className="dropdown dropdown-end ml-3">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+
+              <div className="w-10 rounded-full">
+                <img
+                src={user?.user_metadata?.picture}
+                alt="user-profile"
+                className="rounded-full w-10 h-10 object-cover"
+                />
+              </div>
+
+            </div>
+
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+
+              <Link to={"/userprofile"}>
+                <li>
+                  <a className="justify-between">
+                    Profile
+                  </a>
+                </li>
+              </Link>
+              {/* <li><a>Settings</a></li> */}
+              <li><a onClick={signOut}>Logout</a></li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
 
         {/* lobbies will appear here */}
         <Lobbies/>
