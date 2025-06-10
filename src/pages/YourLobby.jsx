9ef47d1c43bcb96fd05ad7d46eb417b26aacade7
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-06-10 22:48:33 +0800

    adding YourLobby

---
+++ src/pages/YourLobby.jsx
@@ -0,0 +1,7 @@
+export default function YourLobby() {
+  return (
+    <div>
+      {/* to fetch lobbies FROM lobbies(supabase) of each auth user. meaning the User's own lobby */}
+    </div>
+  )
+}
