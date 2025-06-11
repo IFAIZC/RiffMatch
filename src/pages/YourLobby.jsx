d6d61a3a2638348becffa9eaa19361d8c147bbd8
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-06-11 20:41:02 +0800

    x

--- src/pages/YourLobby.jsx
+++ src/pages/YourLobby.jsx
@@ -2,6 +2,7 @@ export default function YourLobby() {
   return (
     <div>
       {/* to fetch lobbies FROM lobbies(supabase) of each auth user. meaning the User's own lobby */}
+      {/*  */}
     </div>
   )
 }
